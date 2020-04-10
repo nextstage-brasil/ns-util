5201a0d5598dbc11d48789da370bc48584931e82
Nextstage DEV <crist@NotePC-Dell>
2020-04-10 16:32:20 -0300

    adicionado ConnectionPostgresql e Metodos ao Helper

---
+++ src/ConnectionPostgreSQL.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace NsUtil;
+
+use Exception;
+use NsUtil\Helper;
+use PDO;
+
+class ConnectionPostgreSQL {
+
+    private $con; // garantir o singleton
+    private $config;
+    public $query;
+    public $result;
+    public $numRows;
+    public $error;
+    public $dd;
+    public $lastInsertId;
+    private static $transaction_in_progress;
+
+    public function __construct($host, $user, $pass, $port, $database) {
+        $this->config = new stdClass();
+        $this->config->host = $host;
+        $this->config->port = $port;
+        $this->config->database = $database;
+        $this->config->user = $user;
+        $this->config->pwd = $pass;
+        $this->open();
+    }
+
+    // gravar o conector em session, vai garantir uma unica conexão no sistema todo, exceto se solicitado explicitamente new
+    public function open() {
+        if (!$this->con) {
+            try {
+                $stringConnection = "pgsql:host=" . $this->config->host . ";port=" . $this->config->port . ";dbname=" . $this->config->database . ";user=" . $this->config->user . ";password=" . $this->config->pwd;
+                $this->con = new PDO($stringConnection);
+                $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $e) {
+                echo '<p class="alert alert-error text-center">'
+                . 'ERROR: Connection Failed (CPD-167)<br/>' . $e->getMessage()
+                . '</p>';
+                die();
+            }
+        }
+    }
+
+    public function getConn() {
+        return $this->con;
+    }
+
+    public function close() {
+        pg_close($this->con);
+    }
+
+    public function begin_transaction() {
+        if (!self::$transaction_in_progress) {
+            $this->executeQuery('START TRANSACTION');
+            self::$transaction_in_progress = true;
+            register_shutdown_function(array($this, "__shutdown_check"));
+        }
+    }
+
+    public function __shutdown_check() {
+        if (self::$transaction_in_progress) {
+            $this->rollback();
+        }
+    }
+
+    public function commit() {
+        $this->executeQuery("COMMIT");
+        self::$transaction_in_progress = false;
+    }
+
+    public function rollback() {
+        $this->executeQuery("ROLLBACK");
+        self::$transaction_in_progress = false;
+    }
+
+    public function autocommit($boolean) {
+        $this->con->autocommit($boolean);
+    }
+
+    private function executeQuery($query) {
+
+        $this->open();
+        $res = false;
+        $this->numRows = 0;
+        $this->result = false;
+        $this->error = false;
+        $this->query = $query;
+
+        try {
+            $this->result = $this->con->prepare($query);
+            if (!$this->result->execute()) {
+                $this->error = $this->result->errorInfo()[2];
+                throw new Exception($this->result->errorInfo()[0] . $this->result->errorInfo()[2], 0);
+            }
+            $this->numRows = $this->result->rowCount();
+        } catch (Exception $exc) {
+            $this->result = false;
+            $this->result = false;
+            if (stripos($exc->getMessage(), 'ERROR:  function unaccent') > -1) {
+                die('DEV:  A EXTESÃO UNNACCENT NÃO FOI INSTALADA');
+            }
+            throw new Exception($exc->getMessage());
+        }
+        return $res;
+    }
+
+    public function next() {
+        try {
+            if ($this->result) {
+                $dados = $this->result->fetch(PDO::FETCH_ASSOC);
+                if (is_array($dados)) {
+                    return $dados;
+                } else {
+                    $this->result = false;
+                    return false;
+                }
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Executa e retonar a query formatada com nameCase
+     * @param type $query
+     * @param type $log
+     * @return type
+     */
+    public function execQueryAndReturn($query, $log = true) {
+        $this->open();
+        $out = [];
+        $this->executeQuery($query, $log);
+        while ($dd = $this->next()) {
+            $out[] = Helper::name2CamelCase($dd);
+        }
+        return $out;
+    }
+
+}
--- src/Helper.php
+++ src/Helper.php
@@ -98,4 +98,116 @@ class Helper {
         });
     }
 
+    /**
+     * Retorna a string no formato camelCase
+     * @param type $string
+     * @param array $prefixo
+     * @return type string
+     */
+    public static function name2CamelCase($string, $prefixo = false): string {
+        $prefixo = array('mem_', 'sis_', 'anz_', 'aux_', 'app_');
+        if (is_array($string)) {
+            foreach ($string as $key => $value) {
+                $out[self::name2CamelCase($key)] = $value;
+            }
+            return $out;
+        }
+        if (is_array($prefixo)) {
+            foreach ($prefixo as $val) {
+                $string = str_replace($val, "", $string);
+            }
+        }
+
+        $string = str_replace('_', ' ', $string);
+        $out = str_replace(' ', '', ucwords($string));
+        $out{0} = mb_strtolower($out{0});
+        return (string) $out;
+    }
+
+    /**
+     * Revert a string camelCase para camel_case
+     * @param type $string
+     * @return type string
+     */
+    public static function reverteName2CamelCase($string): string {
+        $out = '';
+        for ($i = 0; $i < strlen($string); $i++) {
+            if ($string[$i] === mb_strtoupper($string[$i]) && $string[$i] !== '.') {
+                $out .= (($i > 0) ? '_' : '');
+                $string[$i] = mb_strtolower($string[$i]);
+            }
+            $out .= $string[$i];
+        }
+        return (string) $out;
+    }
+
+    /**
+     * Cria a arvore de diretorios
+     * @param type $filename
+     * @return type
+     */
+    private static function createTreeDir($filename) {
+        $path = str_replace('/', DIRECTORY_SEPARATOR, $filename);
+        $parts = explode(DIRECTORY_SEPARATOR, $path);
+        $file = array_pop($parts);
+        $dir = implode(DIRECTORY_SEPARATOR, $parts);
+        @mkdir($dir, 0777, true);
+        return (object) ['path' => $dir, 'name' => $file];
+    }
+
+    public static function saveFile($filename, $name = false, $template = '<?=php Header("Location:/")', $mode = "w+") {
+        $filename = $filename . (($name) ? '/' . $name : '');
+        $file = self::createTreeDir($filename);
+        if (file_exists($filename) && $mode !== 'SOBREPOR') {
+            $file->name = '__NEW__' . $file->name;
+        }
+        $save = str_replace('/', DIRECTORY_SEPARATOR, $file->path . DIRECTORY_SEPARATOR . $file->name);
+        unset($filename);
+        file_put_contents($save, $template);
+        return file_exists($save);
+    }
+
+    public static function eficiencia_init() {
+        list($usec, $sec) = explode(' ', microtime());
+        $ef = new Helper();
+        $ef->start = (float) $sec + (float) $usec;
+        return $ef;
+    }
+
+    public function endEficiencia() {
+        // Terminamos o "contador" e exibimos
+        list($usec, $sec) = explode(' ', microtime());
+        $script_end = (float) $sec + (float) $usec;
+        $elapsed_time = round($script_end - $this->start, 5);
+        $minutos = (int) number_format((double) $elapsed_time / 60, 0);
+
+        return 'Tempo de execução: ' . (($minutos > 0) ? $minutos . 'min' : '') . ceil(($elapsed_time - ($minutos * 60))) . 'segs. Memória utilizada: ' . round(((memory_get_peak_usage(true) / 1024) / 1024), 2) . 'Mb';
+    }
+
+    public static function deleteFile($filepath, $apagarDiretorio = false, $trash = false) {
+        ///echo $filename;
+        $filename = realpath($filepath);
+        $t = explode(DIRECTORY_SEPARATOR, $filename);
+
+        if (is_dir($filename)) {
+            $dir = dir($filename);
+            while ($arquivo = $dir->read()) {
+                if ($arquivo != '.' && $arquivo != '..') {
+                    self::deleteFile($filename . $arquivo, false, $trash);
+                }
+            }
+            $dir->close();
+            if ($apagarDiretorio) {
+                unlink($filename);
+            }
+        } else {
+            if (file_exists($filename)) {
+                unlink($filename);
+            }
+        }
+        if (!file_exists($filename)) {
+            return false;
+        }
+    }
+
 }
