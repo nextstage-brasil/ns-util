fb3fcf3506b9e67ad482fd6b99b9c93784173edd
Nextstage DEV <crist@NotePC-Dell>
2020-06-02 15:07:48 -0300

    Mantendo em sessao o timeout do token recebido

--- src/Integracao/Adapter.php
+++ src/Integracao/Adapter.php
@@ -4,43 +4,72 @@ namespace NsUtil\Integracao;
 
 abstract class Adapter {
 
-    protected $token, $endpoint, $appkey;
+    protected $token, $endpoint, $appkey, $showLogs, $sessionName;
 
-    public function __construct($endpoint, $appkey) {
+    public function __construct($endpoint, $appkey, $autologin = false) {
         $this->endpoint = $endpoint;
         $this->appkey = $appkey;
-        $this->login();
+        if (!$this->sessionName) {
+            $this->sessionName = md5($endpoint . $appkey);
+        }
+        $_SESSION[$this->sessionName] = 0;
+        if ($autologin) {
+            $this->login();
+        }
     }
 
     private function login() {
-        $ret = $this->call('login/enter', [], 'GET', ['App-Key' => $this->appkey]);
-        if ($ret->status === 200 && $ret->error === false && $ret->content->token) {
-            $this->token = $ret->content->token;
-        } else {
-            die('Não foi possível efetuar login com as credenciais informadas');
+        // se validade for menor que agora, faz login
+        if ((int) $_SESSION[$this->sessionName] < time()) {
+            $_SESSION[$this->sessionName] = time() + (1000 * 3); // pra evitar auto loopíng ao usar call
+            $ret = $this->call('login/enter', [], 'GET', ['App-Key' => $this->appkey]);
+            $_SESSION[$this->sessionName] = 0;
+            $this->printLogsOnScreen('Ret-Login: ' . var_export($ret, true));
+            if ($ret->status === 200 && $ret->error === false && $ret->content->token) {
+                $this->token = $ret->content->token;
+            } else {
+                die('Não foi possível efetuar login em "' . $this->endpoint . '" com as credenciais informadas');
+            }
+            $_SESSION[$this->sessionName] = time() + (58 * $ret->content->expire); // duração do token, para não ficar fazendo login toda hora
         }
     }
 
     public function call($recurso, $params = [], $method = 'POST', $header = []) {
-        $url = $this->endpoint . '/' . $recurso;
+        $this->login();
+        try {
+            $url = $this->endpoint . '/' . $recurso;
+            $this->printLogsOnScreen('URL: ' . $url);
 
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request($method, $url, [
-            'verify' => false,
-            'headers' => array_merge(['Token' => $this->token], $header),
-            'json' => $params
-        ]);
+            $client = new \GuzzleHttp\Client();
+            $res = $client->request($method, $url, [
+                'verify' => false,
+                'headers' => array_merge(['Token' => $this->token], $header),
+                'json' => $params
+            ]);
+            $this->printLogsOnScreen('Body: ' . json_encode(json_decode($res->getBody())));
+            $body = \json_decode($res->getBody());
 
-        $body = \json_decode($res->getBody());
 
-
-        $out = new \stdClass();
-        $out->status = $res->getStatusCode();
-        $out->error = (boolean) $body->error;
-        $out->content = $body->content;
-
-        return $out;
+            $out = new \stdClass();
+            $out->status = $res->getStatusCode();
+            $out->error = (boolean) $body->error;
+            $out->content = $body->content;
+            $out->content->token = (($body->token) ? $body->token : false);
+            $out->errorText = (($body->content->error) ? $body->content->error : $body->error);
+            return $out;
+        } catch (Exception $exc) {
+            $out = new \stdClass();
+            $out->status = $res->getStatusCode();
+            $out->error = (boolean) true;
+            $out->content = $exc->getMessage();
+        }
     }
-    
-    
+
+    protected function printLogsOnScreen($text) {
+        if ($this->showLogs) {
+            echo "<hr>printLogAdapterIntegracao: <br/><br/>: ";
+            echo $text . "<hr>";
+        }
+    }
+
 }
