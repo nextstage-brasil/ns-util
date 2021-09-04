c59ff17f1d97d6b3c259488b8931575e9a802f48
Nextstage <ns@nextstage.com.br>
2021-09-04 12:12:11 -0300

    feature/Removido sudo do inicio dos comandos. Se necessário, deve estar na linha do ci ou comando de execução.

--- src/Deployer/lib/deployDefault.sh
+++ src/Deployer/lib/deployDefault.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Configuração do diretorio HOME da aplicação. A pasta www estara abaixo disso
-DIR="/home/nextstag/public_html/trilhasbr.com"
-OWNER="nextstag"
-FILENAME="trilhasbr-backend-package.zip"
-APPNAME="aws_homolog"
+DIR="path"
+OWNER="usuario"
+FILENAME="packageName"
+APPNAME="cliente"
 
 # Sudo para pedir senha caso precise
 cd $DIR
@@ -27,58 +27,56 @@ fi
 
 # directory create
 if [ ! -d ${RELEASES_DIR} ]; then
-     mkdir ${RELEASES_DIR}
+    mkdir ${RELEASES_DIR}
 fi
 if [ ! -d "$DIR/app" ]; then
-     mkdir "$DIR/app"
-     chmod 0777 -R "$DIR/app"
+    mkdir "$DIR/app"
+    chmod 0777 -R "$DIR/app"
 fi
 if [ ! -d "$DIR/.trash" ]; then
-     mkdir "$DIR/.trash"
-     chmod 0777 -R "$DIR/.trash"
+    mkdir "$DIR/.trash"
+    chmod 0777 -R "$DIR/.trash"
 fi
 if [ ! -d "$DIR/storage" ]; then
-     mkdir "$DIR/storage"
-     chmod 0775 -R "$DIR/storage"
-     chown "${OWNER}:www-data" "$DIR/storage"
+    mkdir "$DIR/storage"
+    chmod 0775 -R "$DIR/storage"
+    chown "${OWNER}:www-data" "$DIR/storage"
 fi
 
 
 # clear
- if [ -f "$DIR/www/cron/crontab" ]; then
- rm -R "$DIR/app/.tmp"
- rm -R "$DIR/app/file"
- rm -R "$DIR/app/cookie.txt"
+rm -R "$DIR/app/.tmp"
+rm -R "$DIR/app/file"
+rm -R "$DIR/app/cookie.txt"
 
 
 # Criando diretorio do release
- mkdir ${RELEASE};
- mkdir "$DIR/app/.tmp"
- mkdir "$DIR/app/file"
+mkdir ${RELEASE};
+mkdir "$DIR/app/.tmp"
+mkdir "$DIR/app/file"
 
 # deploy
 echo "- Copiar arquivos"
- unzip -o "$PACKAGE" -d "$RELEASE" > /dev/null
-#  mv "$RELEASE/.htaccess-server" "$RELEASE/.htaccess"
- rm "$PACKAGE"
+unzip -o "$PACKAGE" -d "$RELEASE" > /dev/null
+rm "$PACKAGE"
 
 # versionando o release
 echo "- Versionar release"
 RELEASE_NAME=$(cat "$RELEASE/version") 
- mv $RELEASE "$RELEASES_DIR/$RELEASE_NAME"
+mv $RELEASE "$RELEASES_DIR/$RELEASE_NAME"
 RELEASE="$RELEASES_DIR/$RELEASE_NAME" 
 
 # links simbolicos
 echo "- Criar links"
- ln -nfs "$DIR/app" "$RELEASE/app"
- ln -nfs "$DIR/storage" "$RELEASE/storage"
+ln -nfs "$DIR/app" "$RELEASE/app"
+ln -nfs "$DIR/storage" "$RELEASE/storage"
 # ln -nfs "$DIR/.env" "$RELEASE/.env"
- ln -nfs "$RELEASE" "$DIR/www"
+ln -nfs "$RELEASE" "$DIR/www"
 
 # Permissoes de pastas
- chown -R "${OWNER}:www-data" "$RELEASES_DIR"
- chown "${OWNER}:www-data" "$DIR/app"
- chmod 0777 "$DIR/app" -R
+chown -R "${OWNER}:www-data" "$RELEASES_DIR"
+chown "${OWNER}:www-data" "$DIR/app"
+chmod 0777 "$DIR/app" -R
 
 # Licenciamento e config
 # cp ${DIR}/cs_licence ${RELEASE}/.cs_licence.bkp
@@ -87,26 +85,27 @@ echo "- Criar links"
 # crontab
 if [ -f "$DIR/www/cron/crontab" ]; then
     echo "- Atualizar crontab"
-     chmod -R 0775 "$RELEASE/cron"
-     crontab -l -u ${OWNER} | echo "" |  crontab -u ${OWNER} -
-     crontab -l -u ${OWNER} | cat - "$DIR/www/cron/crontab" |  crontab -u ${OWNER} -
+    chmod -R 0775 "$RELEASE/cron"
+    crontab -l -u ${OWNER} | echo "" | crontab -u ${OWNER} -
+    crontab -l -u ${OWNER} | cat - "$DIR/www/cron/crontab" | crontab -u ${OWNER} -
 fi
 
 # Composer
 if [ -f "$DIR/www/composer.json" ]; then
     echo "- Atualizar pacotes via composer"
-     composer install -q --prefer-dist --optimize-autoloader --no-dev --working-dir="$DIR/www"
+    composer install -q --prefer-dist --optimize-autoloader --no-dev --working-dir="$RELEASE"
+    composer update -q --prefer-dist --optimize-autoloader --no-dev --working-dir="$RELEASE"
 fi
 
 # Manter somente as 5 ultimas versoes
 echo "- Remover releases anteriores"
 cd "$RELEASES_DIR"
- ls -dt ${RELEASES_DIR}/* | tail -n +6 | xargs -d "\n"  rm -rf;
+ls -dt ${RELEASES_DIR}/* | tail -n +6 | xargs -d "\n" rm -rf;
 
 # finalizar
 echo "- Reiniciar serviços"
- service apache2 restart > /dev/null
- service php7.*-fpm restart > /dev/null
+service apache2 restart > /dev/null
+service php7.*-fpm restart > /dev/null
 
 # clear
 echo "\n### Versão $RELEASE_NAME instalada com sucesso!\n"
