8cb1e661da3eaf119d0a29826f7433c9d75e71cf
Nextstage <ns@nextstage.com.br>
2022-02-16 17:53:31 +0000

    feature/Nova classe: LoadCSVToArray

---
+++ src/LoadCSVToArray.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace NsUtil;
+
+class LoadCSVToArray {
+
+    private $file;
+
+    public function __construct($filename) {
+        $this->file = realpath($filename);
+    }
+
+    /**
+     * 
+     * @param string $file_or_dir Diretorio ou CSV que deve ser ingerido
+     * @param string $tablename - Caso false, será utilizado o nome do arquivo CSV sanitizado
+     * @return boolean
+     */
+    public function run() {
+        $types = array('csv', 'xlsx');
+        $ext = strtolower(pathinfo($this->file, PATHINFO_EXTENSION));
+        if (in_array($ext, $types)) {
+            // Caso seja xlsx, converter para CSV usando lib python
+            if ($ext === 'xlsx') {
+                $ret = shell_exec('type xlsx2csv');
+                if (stripos($ret, 'not found') > -1) {
+                    die("### ATENÇÃO ### \nBiblioteca xlsx2csv não esta instalada. "
+                            . "\nPara continuar, execute: 'sudo apt-get update && sudo apt-get install -y xlsx2csv'"
+                            . "\n"
+                            . "###############"
+                            . "\n\n\n");
+                }
+                $t = explode(DIRECTORY_SEPARATOR, $this->file);
+                $csv = '/tmp/' . str_replace('.xlsx', '.csv', array_pop($t));
+                $csv_error = shell_exec("xlsx2csv $file $csv");
+                if (!file_exists($csv)) {
+                    $error = 'Ocorreu erro ao converter arquivo XLSX para CSV: ' . $csv_error;
+                    throw new Exception($error);
+                }
+                $file = $csv;
+            }
+        } else {
+            throw new Exception('Tipo de arquivo não configurado');
+        }
+        if (($handle = fopen($this->file, "r")) !== false) {
+            // Definir explode
+            $fh = fopen($this->file, "rb");
+            $data = fgetcsv($fh, 1000, '\\');
+            fclose($fh);
+            $explode = ',';
+            if (stripos($data[0], ';') > 0) {
+                $explode = ';';
+            }
+            if (stripos($data[0], "\t") > 0) {
+                $explode = "\t";
+            }
+            $out = [];
+            while (($ret = Helper::myFGetsCsv($handle, $explode)) !== false) {
+                $out[] = $ret;
+            }
+
+            return $out;
+        } else {
+            $error = 'Não foi possivel abrir o arquivo indicado';
+            throw new Exception($error);
+        }
+    }
+
+}
