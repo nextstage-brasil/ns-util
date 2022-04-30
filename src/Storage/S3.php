bbebec03936990bf302ceb93ccfd393439be32ea
Nextstage <ns@nextstage.com.br>
2022-04-30 23:48:36 +0100

    feature/URL assinada na AWS para obter arquivos de buckets privado

--- src/Storage/S3.php
+++ src/Storage/S3.php
@@ -10,7 +10,7 @@ class S3 {
 
     public $endpoint;
     private $fs, $adapter;
-    private $key, $secret, $bucket, $region, $version;
+    private $key, $secret, $bucket, $region, $version, $s3Client;
 
     public function __construct($key, $secret, $bucket, $region = 'us-east-2', $version = '2006-03-01') {
         $this->key = $key;
@@ -35,7 +35,7 @@ class S3 {
     }
 
     private function init() {
-        $client = new S3Client([
+        $this->s3Client = new S3Client([
             'credentials' => [
                 'key' => $this->key,
                 'secret' => $this->secret
@@ -44,8 +44,21 @@ class S3 {
             'version' => $this->version,
         ]);
         $this->endpoint = "https://" . $this->bucket . ".s3-" . $this->region . ".amazonaws.com";
-        $this->adapter = new AwsS3Adapter($client, $this->bucket);
+        $this->adapter = new AwsS3Adapter($this->s3Client, $this->bucket);
         $this->fs = new Filesystem($this->adapter);
+    }
+
+    public function getUrlSigned(string $item, int $minutes = 10): string {
+        try {
+            $cmd = $this->s3Client->getCommand('GetObject', [
+                'Bucket' => $this->bucket,
+                'Key' => $item
+            ]);
+            $request = $this->s3Client->createPresignedRequest($cmd, '+' . $minutes . ' minutes');
+            return (string) $request->getUri();
+        } catch (\Exception $exc) {
+            return 'Error on get url signed to item: ' . $exc->getMessage();
+        }
     }
 
 }
