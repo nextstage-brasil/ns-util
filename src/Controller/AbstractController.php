15516bc08325133afbb723a4e7ef3afe16fd0987
Nextstage <ns@nextstage.com.br>
2022-11-17 07:37:07 -0300

    feature/Definição de execução ao receber resposta difernete de 200

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -17,18 +17,16 @@ class AbstractController {
      * @param string $resource
      * @param array $data
      */
-    protected function fetch(string $resource, array $data = [], array $headers = [], string $method = 'GET'): array {
+    protected function fetch(string $resource, array $data = [], array $headers = [], string $method = 'GET', $throwExceptionBasedStatus = true): array {
         $url = $this->config->get('url')
-                . '/'
-                . $resource
-        ;
+            . '/'
+            . $resource;
         $ret = \NsUtil\Helper::curlCall($url, $data, $method, $headers);
-        if ($ret->status > 299 ) {
-            throw new \Exception('Chamada ao recurso ' . $resource . ' com status ' . $ret->status. "\n URL: $url \nMethod: $method \n");
+        if ($ret->status > 299 && $throwExceptionBasedStatus === true) {
+            throw new \Exception('Chamada ao recurso ' . $resource . ' com status ' . $ret->status . "\n URL: $url \nMethod: $method \n");
         }
         $ret->content = json_decode($ret->content);
 
         return json_decode(json_encode($ret), true);
     }
-    
 }
