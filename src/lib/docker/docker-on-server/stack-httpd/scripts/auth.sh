2ef5354df4fd680e616e648a4a7b7f007428b2c5
Nextstage <ns@nextstage.com.br>
2023-02-11 19:39:07 +0100

    bugfix/Correções e melhorias

--- src/lib/docker/docker-on-server/stack-httpd/scripts/auth.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Login na AWS
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ECR}
--- src/lib/docker/scripts/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Verificações iniciais
-cd $(dirname $0);__DIR__=$(pwd)
-source _init.sh
-
-# Down containers
-docker-compose down --volumes --remove-orphans
-docker ps
