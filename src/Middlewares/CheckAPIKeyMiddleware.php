eabbff285ada79dad3e89004675948791f722412
Nextstage <ns@nextstage.com.br>
2023-11-03 16:41:30 +0100

    fix/update

--- src/Middlewares/CheckAPIKeyMiddleware.php
+++ src/Middlewares/CheckAPIKeyMiddleware.php
@@ -4,30 +4,50 @@ namespace NsUtil\Middlewares;
 
 use Exception;
 use NsUtil\Api;
-use NsUtil\Exceptions\RedisConnectionException;
-use NsUtil\Exceptions\TooManyRequestException;
 use NsUtil\Helper;
 use NsUtil\Interface\MiddlewareInterface;
-use NsUtil\Services\RateLimiter;
 
-use function NsUtil\dd;
-
+/**
+ * CheckAPIKeyMiddleware validates the presence of API keys for secured routes and implements the MiddlewareInterface.
+ */
 class CheckAPIKeyMiddleware implements MiddlewareInterface
 {
+    /**
+     * The API key used for authentication and access to secured routes.
+     *
+     * @var string|null
+     */
     public ?string $apikey = null;
 
-    // Rotas liberadas sem precisar de autenticação de usuário
+    /**
+     * The list of routes that do not require user authentication for access.
+     *
+     * @var array
+     */
     public array $freeRoutes = ['version', 'cpuAgent', 'healtcheck'];
 
+    /**
+     * The instance of the API handler.
+     *
+     * @var Api
+     */
     protected Api $api;
 
+    /**
+     * Constructs the CheckAPIKeyMiddleware.
+     */
     public function __construct()
     {
     }
 
+    /**
+     * Handles the API request by validating the API key for secured routes.
+     *
+     * @param Api $api - The API instance to handle the request.
+     * @return bool - Returns true on successful API key validation for secure routes.
+     */
     public function handle(Api $api): bool
     {
-
         $this->api = $api;
 
         $this->setApiKey();
@@ -35,13 +55,16 @@ class CheckAPIKeyMiddleware implements MiddlewareInterface
         $resource = mb_strtolower(Helper::name2CamelCase($api->getConfigData()['rest']['resource']));
         $this->freeRoutes = array_map(fn ($item) => mb_strtolower(Helper::name2CamelCase($item)), $this->freeRoutes);
 
-
         if (in_array($resource, $this->freeRoutes)) {
             return true;
         }
 
         return $this->check();
     }
+
+    /**
+     * Sets the API key for the middleware.
+     */
     public function setApiKey(): void
     {
         $headers = $this->api->getHeaders(true);
@@ -50,8 +73,14 @@ class CheckAPIKeyMiddleware implements MiddlewareInterface
             ?? $this->apikey;
     }
 
+    /**
+     * Validates the API key for secure routes. This method should be implemented in the final class.
+     *
+     * @return bool - Returns true if the API key validation succeeds.
+     * @throws Exception - Throws an exception since this method is not implemented in this abstract class.
+     */
     public function check(): bool
     {
-        throw new Exception('Check API not implemented in final class');
+        throw new Exception('Check API not implemented in the final class');
     }
 }
--- src/Middlewares/RateLimitMiddleware.php
+++ src/Middlewares/RateLimitMiddleware.php
@@ -10,22 +10,67 @@ use NsUtil\Helper;
 use NsUtil\Interface\MiddlewareInterface;
 use NsUtil\Services\RateLimiter;
 
+/**
+ * RateLimitMiddleware provides a rate-limiting mechanism for API requests and implements MiddlewareInterface for request handling.
+ */
 class RateLimitMiddleware implements MiddlewareInterface
 {
+    /**
+     * The maximum limit of API calls allowed within a specific time interval.
+     *
+     * @var int
+     */
     public int $maxCallsLimit = 60;
+
+    /**
+     * The time interval (in seconds) within which the maximum calls limit is considered.
+     *
+     * @var int
+     */
     public int $secondsInterval = RateLimiter::PER_MINUTE;
+
+    /**
+     * The API key for identifying and limiting requests (defaulted to 'not-defined').
+     *
+     * @var string|null
+     */
     public ?string $apikey = 'not-defined';
 
+    /**
+     * An associative array to configure rate limits by different API routes.
+     * For example: 'healthcheck' => [200, RateLimiter::PER_MINUTE]
+     *
+     * @var array
+     */
     public array $configByRoute = [];
 
+    /**
+     * A flag indicating whether the API key is set for rate limiting.
+     *
+     * @var bool
+     */
     public bool $setApiKey = false;
 
+    /**
+     * The instance of the API handler.
+     *
+     * @var Api
+     */
     private Api $api;
 
+    /**
+     * Constructs the RateLimitMiddleware.
+     */
     public function __construct()
     {
     }
 
+    /**
+     * Handles the API request by applying rate limiting rules and verifying API keys (if configured).
+     *
+     * @param Api $api - The API instance to handle the request.
+     * @return bool - Returns true on successful request handling.
+     */
     public function handle(Api $api): bool
     {
         $this->api = $api;
@@ -39,18 +84,17 @@ class RateLimitMiddleware implements MiddlewareInterface
             ])
         );
 
-        $maxCallsLimit = $this->configByRoute[$resource][0] ?? $this->maxCallsLimit;
-        $secondsInterval = $this->configByRoute[$resource][1] ?? $this->secondsInterval;
+        $this->maxCallsLimit = $this->configByRoute[$resource][0] ?? $this->maxCallsLimit;
+        $this->secondsInterval = $this->configByRoute[$resource][1] ?? $this->secondsInterval;
 
         $this->setApiKey();
 
         $this->check();
-
         try {
             if ($this->setApiKey && null !== $this->apikey) {
-                RateLimiter::byKey($this->apikey, $maxCallsLimit, $secondsInterval);
+                RateLimiter::byKey($this->apikey, $this->maxCallsLimit, $this->secondsInterval);
             } else {
-                RateLimiter::byIP($maxCallsLimit, $secondsInterval);
+                RateLimiter::byIP($this->maxCallsLimit, $this->secondsInterval);
             }
         } catch (RedisConnectionException $exc) {
             $this->api->error($exc->getMessage(), $exc->getCode());
@@ -63,6 +107,9 @@ class RateLimitMiddleware implements MiddlewareInterface
         return true;
     }
 
+    /**
+     * Sets the API key for rate limiting.
+     */
     public function setApiKey(): void
     {
         $headers = $this->api->getHeaders(true);
@@ -75,6 +122,11 @@ class RateLimitMiddleware implements MiddlewareInterface
             : $this->apikey;
     }
 
+    /**
+     * Checks if the request passes the rate-limiting criteria.
+     *
+     * @return bool - Returns true if the request passes the rate-limiting check.
+     */
     public function check(): bool
     {
         return true;
